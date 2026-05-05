4b99abe2bbcedd436c3de2faa34546f8c6a0312f
HeyitsMelisa <146728346+HeyitsMelisa@users.noreply.github.com>
2026-05-05 19:20:25 +0200

    Update To-Do.java

--- To-Do.java
+++ To-Do.java
@@ -116,17 +116,28 @@ import java.util.List;
 enum Priority {
     // TODO:
     // Define the enum values:
-    // LOW
-    // MEDIUM
-    // HIGH
+     LOW,
+     MEDIUM,
+     HIGH
 }
 
 class Task {
+   private String title;
+   private String description;
+   private enum priority;
+   private LocalDate dueDate;
 
     // TODO:
     // Define all required attributes
 
     public Task(String title, String description, Priority priority, LocalDate dueDate) {
+       this.title=title;
+       this.description=description;
+       this.priority=priority;
+       this.dueDate=dueDate;
+
+       dueDate= 10.11.2011;
+       
         // TODO:
         // Validate input
         // Initialize attributes
@@ -134,22 +145,22 @@ class Task {
 
     public String getTitle() {
         // TODO:
-        return null;
+        return title;
     }
 
     public String getDescription() {
         // TODO:
-        return null;
+        return description;
     }
 
     public Priority getPriority() {
         // TODO:
-        return null;
+        return priority;
     }
 
     public LocalDate getDueDate() {
         // TODO:
-        return null;
+        return dueDate;
     }
 
     public boolean isDone() {
@@ -158,51 +169,68 @@ class Task {
     }
 
     public void markDone() {
-        // TODO:
-        // Set task status to done
+        // TODO
+       if(isDone()){
+          return true;
+       }
+       return false;
     }
 
     @Override
     public String toString() {
         // TODO:
         // Return a readable representation of the task
-        return null;
+        return "Title: " + getTitle() + " Description:" + getDescription() + "PrioLevel:" + getPriority() + "Due:" + getDueDate();
+           
     }
 }
 
 class TaskManager {
 
     // TODO:
-    // Define the task list
+    List<Task> taskList = new List<Task>();
 
     public void addTask(Task task) {
         // TODO:
-        // Add task to the list
+        taskList.add(task);
     }
 
     public void removeTask(String title) {
         // TODO:
-        // Remove a task by title
+        taskList.remove(title);
     }
 
     public void markTaskDone(String title) {
         // TODO:
         // Find a task by title and mark it as done
+       for(int i=0;i<taskList.size();i++){
+          if(taskList.get(i).equals(title)){// oder taskList.get(i) == title
+             return title.isDone();// oder einfach nur isDone()?? Wenn ja why, weil woher soll die Methode dann wissen welcher titel is done sein muss
     }
 
     public void printAllTasks() {
         // TODO:
         // Print all tasks
+       for(int i=0;i<taskList.size();i++){
+          System.out.println(taskList.get(i));
     }
 
     public void printOpenTasks() {
         // TODO:
         // Print only tasks that are not done
+       for(int i=0;i<taskList.size();i++){
+          if(!(taskList.get(i).isDone())){ //oder taskList.get(i).isDone()==false
+            System.out.println(taskList.get(i));
+          }   
     }
 
     public void printCompletedTasks() {
         // TODO:
         // Print only completed tasks
+        for(int i=0;i<taskList.size();i++){
+          if(taskList.get(i).isDone()){ //oder taskList.get(i).isDone()==true
+            System.out.println(taskList.get(i));
+          } 
     }
 
     public void sortByPriority() {
@@ -218,7 +246,18 @@ class TaskManager {
 
 public class Main {
     public static void main(String[] args) {
+       List<Task> lst1=new List<Task>();
+       lst1.add("Outlook");
+       lst1.add("Copilot");
+       lst1.add("Osu");
+       lst1.add("League of Ledgends");
+       lst1.add("Counterstrike");
 
+       lst1.get("Outlook").markAsDone();
+       lst1.get("Osu").markAsDone();
+
+       lst1.printOpenTasks();
+       lst1.printCompletedTasks();
         /*
         TODO:
         1. Create a TaskManager object
